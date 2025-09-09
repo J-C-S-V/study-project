5d3c177c8d28389e9462dca5813cffbdbdc1057c
Juan Sanchez <juansan1993@hotmail.com>
2025-09-08 22:37:59 -0500

    Update Github logo

--- redux-review-five/src/App.jsx
+++ redux-review-five/src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import { LogoGithub } from "./assets/icons/logo";
 
+const MyComponent = () => {
+  return (
+<>
+<h1>hihihi</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, cum consequatur? Blanditiis exercitationem perspiciatis velit facere asperiores, quas dolores culpa qui nam maxime? Culpa facilis dignissimos quo accusantium quas cum?</p>
+</>
+  )
+};
+
 function App() {
   return (
     <>
       <h1>Hello Juan!</h1>
+      <MyComponent />
       <LogoGithub />
     </>
   );
