65402871b812c0e3020f91a2c6c73990cb7cb1cc
Juan Sanchez <juansan1993@hotmail.com>
2025-09-11 11:47:51 -0500

    Add decrement and increment buttons

--- Redux-review/src/App.tsx
+++ Redux-review/src/App.tsx
@@ -7,8 +7,6 @@ import {
 } from "./store/slices/counter";
 import "./App.css";
 
-// import { increment, decrement } from "./store/slices/counter";
-
 const selectCounterValue = (state: RootState) => state.counter.value;
 
 function App() {
@@ -26,6 +24,13 @@ function App() {
       <button onClick={() => dispatch(incrementByTwo())}>Increment by 2</button>
       <button onClick={() => dispatch(incrementByAmount(INCREMENT_AMOUNT))}>
         Increment by {INCREMENT_AMOUNT}
+      </button>
+      {/* New buttons */}
+      <button onClick={() => dispatch(incrementByAmount(10))}>
+        Increment by 10
+      </button>
+      <button onClick={() => dispatch(decrement())}>
+        Decrement Again
       </button>
     </>
   );
