092fe8edac876a1bf70e3bf7bc0e66580deb381d
Juan Sanchez <juansan1993@hotmail.com>
2025-09-11 11:51:45 -0500

    Fix Increment button bug

--- Redux-review/src/App.tsx
+++ Redux-review/src/App.tsx
@@ -7,13 +7,11 @@ import {
 } from "./store/slices/counter";
 import "./App.css";
 
-const selectCounterValue = (state: RootState) => state.counter.value;
+// import { increment, decrement } from "./store/slices/counter";
 
 function App() {
-  const value = useAppSelector(selectCounterValue);
+  const value = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
-
-  const INCREMENT_AMOUNT = 5;
 
   return (
     <>
@@ -22,15 +20,8 @@ function App() {
       <button onClick={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(incrementByTwo())}>Increment by 2</button>
-      <button onClick={() => dispatch(incrementByAmount(INCREMENT_AMOUNT))}>
-        Increment by {INCREMENT_AMOUNT}
-      </button>
-      {/* New buttons */}
-      <button onClick={() => dispatch(incrementByAmount(10))}>
-        Increment by 10
-      </button>
-      <button onClick={() => dispatch(decrement())}>
-        Decrement Again
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment by 5
       </button>
     </>
   );
