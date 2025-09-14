e3848f56a724584fe12e68df594269278b948087
Juan Sanchez <juansan1993@hotmail.com>
2025-09-13 23:42:56 -0500

    Add Navbar component

---
+++ Redux-review/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <nav>
+      <div>Logo</div>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </nav>
+  )
+}
