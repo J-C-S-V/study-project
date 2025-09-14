dbb5ced92dd784990d041f77d790e0c7cdebb805
Juan Sanchez <juansan1993@hotmail.com>
2025-09-13 23:44:39 -0500

    Add components to main file

--- Redux-review/src/main.tsx
+++ Redux-review/src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <Navbar />
       <App />
+      <Footer />
     </Provider>
   </StrictMode>
 );
