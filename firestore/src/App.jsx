b99e5924c6b5ae78a2771822aecc304131883563
Juan Sanchez <juansan1993@hotmail.com>
2025-09-16 16:45:01 -0500

    Add firebase logic to fetch data

--- firestore/src/App.jsx
+++ firestore/src/App.jsx
@@ -1,14 +1,24 @@
-
-import { useEffect } from 'react'
+import { db } from './lib/firebaseConfig'
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [ name, setName ] = useState('');
 
+  useEffect(() => {
+    async function fetchFirebaseData() {
+      const docRef = doc(db, 'firebaseCollection', 'firebaseDocument')
+      const docSnap = await getDoc(docRef)
+      setName(docSnap.data().name)
+    }
 
+    fetchFirebaseData();
+  }, [])
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello {name}</h1>
     </>
   )
 }
