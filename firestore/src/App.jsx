9ac4901b8b12cd9e278025df3731cf997baaf547
Juan Sanchez <juansan1993@hotmail.com>
2025-09-16 20:40:08 -0500

    Add signIn and signUp buttons

--- firestore/src/App.jsx
+++ firestore/src/App.jsx
@@ -1,6 +1,8 @@
 import { db } from './lib/firebaseConfig'
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from 'react'
+import { auth } from './lib/firebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import './App.css'
 
 function App() {
@@ -10,15 +12,49 @@ function App() {
     async function fetchFirebaseData() {
       const docRef = doc(db, 'firebaseCollection', 'firebaseDocument')
       const docSnap = await getDoc(docRef)
-      setName(docSnap.data().name)
+      
+      if (docSnap.exists()) {
+        setName(docSnap.data().name);
+      } else {
+        console.log("No such document!");
+      }
     }
 
     fetchFirebaseData();
   }, [])
 
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, 'email@test.com', 'password')
+    .then((response) => {
+      console.log('response', response)
+    })
+    .catch((error) => {
+      console.error('Error signing up', error)
+    })
+
+  }
+
+  
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, 'email@test.com', 'password')
+    .then((response) => {
+      console.log('Sign in response', response)
+    })
+    .catch((error) => {
+      console.error('Error signing in', error)
+    })
+  }
+
+  
+  const logOut = () => {
+    
+  }
+
   return (
     <>
       <h1>Hello {name}</h1>
+      <button onClick={signUp}>Sign Up</button>
+      <button onClick={signIn}>Sign In</button>
     </>
   )
 }
