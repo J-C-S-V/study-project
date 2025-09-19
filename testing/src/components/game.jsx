2f222f6abdd87d52ddca3cd0e9b8789e3aad255e
Juan Sanchez <juansan1993@hotmail.com>
2025-09-19 12:55:56 -0500

    Add winner function

--- testing/src/components/game.jsx
+++ testing/src/components/game.jsx
@@ -1,78 +1,84 @@
 import { useState } from "react";
 
-export function Square({ index, value, handleClickMark }) {
+export function Square({ value, onSquareClick }) {
   return (
-    <>
-      <div onClick={handleClickMark} className="square">
-        {value}
-      </div>
-    </>
+    <div onClick={onSquareClick} className="square">
+      {value}
+    </div>
   );
 }
 
 export function Board() {
-  const values = {
-    X: "X",
-    O: "O",
-    none: null,
+  const [squares, setSquares] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  const [isNextPlayer, setIsNextPlayer] = useState(true);
+
+  const handleClickValue = (index) => {
+    if (squares[index] || checkWinner(squares)) {
+      return;
+    }
+    const nextSquares = [...squares];
+    if (isNextPlayer) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+    setSquares(nextSquares);
+    setIsNextPlayer(!isNextPlayer);
+    checkWinner(nextSquares);
   };
 
-  const boardState = ["X", null, "O", null, null, null, null, null, null];
+  function checkWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  const [value, setValue] = useState("");
-  const [board, setBoard] = useState(boardState);
-
-  // function detectWinner() {
-  //   if(0 and 1 and 2) {
-  //     setWinner('Winner!')
-  //   }
-  // }
-
-  const handleClickValue = (e) => {
-    setValue(values.O);
-    console.log(e.target);
-  };
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
   return (
     <div className="board">
       <div className="row">
-        <Square handleClickValue={handleClickValue} index={0} value={value} />
-        <Square handleClickValue={handleClickValue} index={1} value={value} />
-        <Square handleClickValue={handleClickValue} index={2} value={value} />
+        <Square onSquareClick={() => handleClickValue(0)} value={squares[0]} />
+        <Square onSquareClick={() => handleClickValue(1)} value={squares[1]} />
+        <Square onSquareClick={() => handleClickValue(2)} value={squares[2]} />
       </div>
       <div className="row">
-        <Square
-          handleClickValue={handleClickValue}
-          index={3}
-          value={boardState[3]}
-        />
-        <Square
-          handleClickValue={handleClickValue}
-          index={4}
-          value={boardState[4]}
-        />
-        <Square
-          handleClickValue={handleClickValue}
-          index={5}
-          value={boardState[5]}
-        />
+        <Square onSquareClick={() => handleClickValue(3)} value={squares[3]} />
+        <Square onSquareClick={() => handleClickValue(4)} value={squares[4]} />
+        <Square onSquareClick={() => handleClickValue(5)} value={squares[5]} />
       </div>
       <div className="row">
-        <Square
-          handleClickValue={handleClickValue}
-          index={6}
-          value={boardState[6]}
-        />
-        <Square
-          handleClickValue={handleClickValue}
-          index={7}
-          value={boardState[7]}
-        />
-        <Square
-          handleClickValue={handleClickValue}
-          index={8}
-          value={boardState[8]}
-        />
+        <Square onSquareClick={() => handleClickValue(6)} value={squares[6]} />
+        <Square onSquareClick={() => handleClickValue(7)} value={squares[7]} />
+        <Square onSquareClick={() => handleClickValue(8)} value={squares[8]} />
       </div>
     </div>
   );
