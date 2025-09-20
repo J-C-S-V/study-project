56ade212f9965caaaf917b79ce86d25c3fe817a3
Juan Sanchez <juansan1993@hotmail.com>
2025-09-19 21:39:33 -0500

    Add handleSquareClick

--- testing/src/components/game.jsx
+++ testing/src/components/game.jsx
@@ -1,1 +1,41 @@
 import { useState } from "react";
+
+const Square = ({ onSquareClick, value }) => {
+  return (
+    <>
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    </>
+  );
+};
+
+export const Board = () => {
+  const [matrix, setMatrix] = useState(Array(9).fill(null));
+
+  const handleSquareClick = (index) => {
+    const matrixCopy = [...matrix];
+    matrixCopy[index] = "X";
+    setMatrix(matrixCopy);
+  };
+
+  return (
+    <div className="board">
+      <div className="row">
+        <Square value={matrix[0]} onSquareClick={() => handleSquareClick(0)} />
+        <Square value={matrix[1]} onSquareClick={() => handleSquareClick(1)} />
+        <Square value={matrix[2]} onSquareClick={() => handleSquareClick(2)} />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+};
